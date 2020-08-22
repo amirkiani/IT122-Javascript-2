69eb2c85c9c9fabdffd541c2b24af1aafa5ea228
Ameer Kiani <ameerkiani@icloud.com>
2020-08-22 01:59:10 -0700

    fixed some issues

--- index.js
+++ index.js
@@ -23,67 +23,86 @@ app.use(express.static(__dirname + '/public' ));
 
 // Parser for form submissions
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use('/api', require('cors')());
 // Create variable to get data from data.js
-let showBooks = books.getAll();
 
+app.get('/', (req, res, next) => {
+    return Book.find({}).lean()
+        .then((books) => {
+            res.render('home-react', {books: JSON.stringify(books)});
+        })
+        .catch(err => next(err));
+})
 
 app.get('/api/books', (req, res) => {
-    return Book.find({}).lean()
-      .then((books) => {
-          res.render('home', { books });
-      })
-      .catch(err => next(err));
-  });
+    return Book.find({})
+    .then((books) => {
+        res.json(books)
+    })
+    .catch(err => {
+        res.status(500).send('Database error')})
+})
 
 app.get('/detail', (req, res) => {
-    const booktitle = req.query.title
-    Book.findOne({"title": booktitle }).lean()
-  .then((book) => {
-      console.log(book);
-      res.render('detail', {title: booktitle, stats: book});
-  })
-  .catch(err => next(err));
-});
-
-
-
-app.get('/delete', (req, res) => {
     const booktitle = req.query.title;
-    books.findByIDAndDelete({title: booktitle}, (err, book) => {
-        
-        if (err) {
-            console.log(err);
-        } else if (!book) {
-            console.log(booktitle + "Book not found");
-            res.send(`${booktitle} not found`);
-        } else if (book) {
-            console.log(booktitle + "Removed");
-            res.send(`${booktitle} Removed`);
-        }
+    Book.findOne({title: booktitle}).lean()
+    .then((books) => {
+        res.render('detail', {title: booktitle, stats: books});
     });
 });
 
-
-app.delete('/api/books/:title', (req, res) => {
+app.get('/api/books/:title', (req, res) => {
     const booktitle = req.params.title; 
-    books.findOneAndDelete({title: booktitle})
-    .then(book => {
-        if(book === null) {
-            return res.status(400).send(`Error: "${booktitle}" not found`)   
+    Book.findOne({title: booktitle})
+    .then((book) => {
+        if (book === null) {
+            return res.status(400).send(`Error: "${booktitle}" not found`)
         } else {
-            res.json(book)}
+        res.json(book)
+        }
+    })
+    .catch(err => {
+        res.status(500).send('Database error', err)
+    })
     })
 
-    .catch(err => {
-        res.status(500).send('Dabatase error', err)
+
+app.get('/delete', (req, res) => {
+        const booktitle = req.query.title;
+        Book.findOneAndDelete({title: booktitle}, (err, book) => {
+            if (err) {
+                console.log(err);
+            } else if (!book) {
+                console.log("Unable to find" + booktitle);
+                res.send(`${booktitle} not found`);
+            } else if (book) {
+                console.log("Deletion of " + booktitle + "successful");
+                res.send(`${booktitle} delete successful`);
+            }
+        });
+    });
+
+    app.get('/api/books/delete/:title', (req, res) => {
+        const booktitle = req.params.title; 
+        Book.findOneAndDelete({title: booktitle})
+        .then(book => {
+            if(book === null) {
+                return res.status(400).send(`Error: "${booktitle}" not found`)   
+            } else {
+                res.json(book)}
+        })
+    
+        .catch(err => {
+            res.status(500).send('Database Error', err)
+        })
     })
-})
+    
 
 app.post('/api/books/:title', (req, res) => {
     const booktitle = req.params.title;
-    books.findOneAndUpdate({title: booktitle}, req.body, {upsert: true, new: true})
+    Book.findOneAndUpdate({title: booktitle}, req.body, {upsert: true, new: true})
     .then(book => {
         res.json(book)
     })
